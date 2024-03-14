5b61bdbba24cd604e28cfdcce087ea421b98140e
Daniel <danzaric10@gmail.com>
2024-03-14 17:38:39 +0100

    Added Vue3-moveable package
    More on https://www.npmjs.com/package/vue3-moveable, examples on: https://daybrush.com/moveable/storybook/index.html?path=/story/basic--basic-draggable

--- pages/dashboard/albums/[id]/pages/[pageId].vue
+++ pages/dashboard/albums/[id]/pages/[pageId].vue
@@ -2,9 +2,54 @@
   <div class="h-full flex flex-col">
     <AdminHeader></AdminHeader>
 
-    <div class="h-full grid grid-cols-12 bg-base-tertiary p-4">
+    <div class="h-full grid grid-cols-12 bg-base-tertiary p-4 relative">
       <div class="h-full col-span-9 flex justify-center gap-x-2">
-        <div class="h-full aspect-thumbnail bg-base-white"></div>
+        <div ref="container" class="relative aspect-thumbnail bg-base-white">
+          <div class="target w-[100px] aspect-thumbnail bg-base-tertiary"></div>
+          <div
+            class="titi w-[100px] aspect-thumbnail bg-base-tertiary mt-4"
+          ></div>
+          <Moveable
+            :target="['.target', '.titi']"
+            :individual-groupable="true"
+            :container="container"
+            :draggable="true"
+            :rotatable="true"
+            :throttle-rotate="90"
+            :snappable="true"
+            :snap-container="container"
+            :snap-threshold="25"
+            :snap-grid-height="40"
+            :snap-grid-width="40"
+            :snap-directions="{
+              top: true,
+              left: true,
+              bottom: true,
+              right: true,
+            }"
+            :bounds="{
+              left: 0,
+              top: 0,
+              right: 0,
+              bottom: 0,
+              position: 'css',
+            }"
+            :is-display-snap-digit="true"
+            :is-display-inner-snap-digit="false"
+            :elementGuidelines="['.target', '.titi', '.element3']"
+            :snap-gap="true"
+            :element-snap-directions="{
+              top: true,
+              left: true,
+              bottom: true,
+              right: true,
+              center: true,
+              middle: true,
+            }"
+            @drag="onDrag"
+            @rotate="onRotate"
+          />
+        </div>
         <div class="h-full flex flex-col justify-between">
           <div class="flex flex-col gap-y-2">
             <div v-for="option in options" class="bg-base-white rounded-md">
@@ -20,7 +65,7 @@
               <Icon
                 name="i-mdi:cancel-box-outline"
                 size="42"
-                class="border-2 border-transparent rounded-md hover:border-black hover:cursor-pointer hover:text-red-700"
+                class="border-2 border-transparent rounded-md hover:border-red-700 hover:cursor-pointer hover:text-red-700"
               />
             </div>
           </div>
@@ -43,11 +88,15 @@
 </template>
 
 <script lang="ts" setup>
+  import Moveable from "vue3-moveable";
+
   definePageMeta({
     layout: "dashboard",
   });
 
   const { t } = useI18n();
+  const container = ref<HTMLElement | null>(null);
+  const moveable = ref(null);
 
   const options = [
     {
@@ -67,6 +116,50 @@
       description: t("admin-sticker-remove-option"),
     },
   ];
+
+  const bounds = computed((): DOMRect | undefined => {
+    return container.value?.getBoundingClientRect();
+  });
+
+  const onDrag = ({
+    target,
+    transform,
+    left,
+    top,
+    right,
+    bottom,
+    beforeDelta,
+    beforeDist,
+    delta,
+    dist,
+    clientX,
+    clientY,
+  }) => {
+    //console.log("onDrag: ", e.target)
+    console.log("onDrag left, top", left, top);
+    target!.style.left = `${left}px`;
+    target!.style.top = `${top}px`;
+    target!.style.transform = transform;
+  };
+
+  const onRotate = ({ target, transform }) => {
+    console.log("rottattig", transform);
+    target!.style.transform = transform;
+  };
+
+  const onBound = (e) => {
+    console.log(e);
+  };
+
+  // onMounted(() => {
+  //   nextTick(() => {
+  //     console.log("onMounted nextTick", moveable.value);
+  //     // moveable.value.updateSelectors();
+  //     // moveable.value.forceUpdate();
+  //     // moveable.value.updateTarget();
+  //     // moveable.value.updateRect();
+  //   });
+  // });
 </script>
 
 <style></style>
