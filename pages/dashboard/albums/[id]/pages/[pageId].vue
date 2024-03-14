89707da7b469d668162e116758a1fb1816470169
Daniel <danzaric10@gmail.com>
2024-03-14 14:20:12 +0100

    Added simple layout for adding stickers

--- pages/dashboard/albums/[id]/pages/[pageId].vue
+++ pages/dashboard/albums/[id]/pages/[pageId].vue
@@ -1,6 +1,44 @@
 <template>
   <div class="h-full flex flex-col">
     <AdminHeader></AdminHeader>
+
+    <div class="h-full grid grid-cols-12 bg-base-tertiary p-4">
+      <div class="h-full col-span-9 flex justify-center gap-x-2">
+        <div class="h-full aspect-thumbnail bg-base-white"></div>
+        <div class="h-full flex flex-col justify-between">
+          <div class="flex flex-col gap-y-2">
+            <div v-for="option in options" class="bg-base-white rounded-md">
+              <Icon
+                :name="option.icon"
+                size="42"
+                class="border-2 border-transparent rounded-md hover:border-black hover:cursor-pointer"
+              />
+            </div>
+          </div>
+          <div>
+            <div class="bg-base-white rounded-md">
+              <Icon
+                name="i-mdi:cancel-box-outline"
+                size="42"
+                class="border-2 border-transparent rounded-md hover:border-black hover:cursor-pointer hover:text-red-700"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="h-full col-span-3 p-4 flex flex-col items-center bg-white">
+        <h2 class="text-xl font-bold">Stickers</h2>
+        <div class="h-full flex flex-col justify-center gap-y-8">
+          <div v-for="option in options" class="flex flex-col items-center">
+            <div class="p-2 border-2 border-base-tertiary rounded-full">
+              <Icon :name="option.icon" size="42" />
+            </div>
+            <span class="text-sm">{{ option.description }}</span>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,6 +46,27 @@
   definePageMeta({
     layout: "dashboard",
   });
+
+  const { t } = useI18n();
+
+  const options = [
+    {
+      icon: "i-fluent:sticker-add-20-filled",
+      description: t("admin-sticker-add-option"),
+    },
+    {
+      icon: "i-mdi:file-gif-box",
+      description: t("admin-sticker-add-gif-option"),
+    },
+    {
+      icon: "i-mage:sound-waves",
+      description: t("admin-sticker-add-audio-option"),
+    },
+    {
+      icon: "i-fluent:sticker-20-filled",
+      description: t("admin-sticker-remove-option"),
+    },
+  ];
 </script>
 
 <style></style>
